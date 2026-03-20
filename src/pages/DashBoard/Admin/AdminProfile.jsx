a6fdaa09216e99a6aaaed2c2b9f90a276f464d8c
Junak12 <pauljunak@gmail.com>
2026-03-20 21:17:18 +0600

    create a jsx for admin Profile

--- src/pages/DashBoard/Admin/AdminProfile.jsx
+++ src/pages/DashBoard/Admin/AdminProfile.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Profile from '../User/Profile'
 
 const AdminProfile = () => {
   return (
-    <div className='pl-80'>
-      Hello Admin
+    <div>
+      <Profile/>
     </div>
   )
 }
